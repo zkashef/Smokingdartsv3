330364f08768c99ab2f0bf00d7bd335da169852a
Zach <zkashef@icloud.com>
2023-03-26 16:51:07 -0400

    changes or something

--- camera_test.py
+++ camera_test.py
@@ -11,23 +11,39 @@ def capture_image(camera, image_name, image_path):
     print(image_name + " captured!")
 
 
+# loop through possible camera indexes
+#for i in range(10):
+#    cap = cv2.VideoCapture(i)
+#    # check if camera was opened successfully
+#    if cap.read()[0]:
+#        print(f"Camera index {i} is available")
+#        cap.release()
 
 # set directory paths
 cur_dir = os.getcwd()
 parent_dir = os.path.dirname(cur_dir)
 print("current dir: ", cur_dir)
 print("parent dir: ", parent_dir)
-image_path = os.path.dirname(os.getcwd()) + "/images/test"
+image_path = cur_dir + "/images/test"
 print("image path: ", image_path)
 
 # initialize camera objects
 camX = cv2.VideoCapture(0) # First camera connected to USB port 0
-camY = cv2.VideoCapture(1) # Second camera connected to USB port 1
+print("camx works")
+camY = cv2.VideoCapture(2) # Second camera connected to USB port 1
+print("camy works")
 
 # Capture images of dartboard with no dart
 input("\nPress enter to capture image of dartboard with no dart")
 capture_image(camX, "image_nodartX", image_path)
 capture_image(camY, "image_nodartY", image_path)
+
+
+# initialize camera objects
+camX = cv2.VideoCapture(0) # First camera connected to USB port 0
+print("camx works")
+camY = cv2.VideoCapture(2) # Second camera connected to USB port 1
+print("camy works")
 
 # Capture images of dartboard with dart
 input("\nPress enter to capture image of dartboard with dart")
--- proof_of_scoring_script.py
+++ proof_of_scoring_script.py
@@ -68,22 +68,22 @@ if __name__ == "__main__":
     # set directory paths
     cur_dir = os.getcwd()
     parent_dir = os.path.dirname(cur_dir)
+    image_path = cur_dir + "/images/test"
     print("current dir: ", cur_dir)
     print("parent dir: ", parent_dir)
-    image_path = os.path.dirname(os.getcwd()) + "/images/test"
     print("image path: ", image_path)
 
-
-    # initialize camera objects
-    camX = cv2.VideoCapture(1) # First camera connected to USB port 0
+    # initialize camera objects and capture images of dartboard with no dart
+    camX = cv2.VideoCapture(0) # First camera connected to USB port 0
     camY = cv2.VideoCapture(2) # Second camera connected to USB port 1
-
-    # Capture images of dartboard with no dart
     input("\nPress enter to capture image of dartboard with no dart")
     capture_image(camX, "image_nodartX", image_path)
     capture_image(camY, "image_nodartY", image_path)
 
-    # Capture images of dartboard with dart
+
+    #  Reinitialize camera objectsCapture images of dartboard with dart
+    camX = cv2.VideoCapture(0) # First camera connected to USB port 0
+    camY = cv2.VideoCapture(2) # Second camera connected to USB port 1
     input("\nPress enter to capture image of dartboard with dart")
     capture_image(camX, "image_dartX", image_path)
     capture_image(camY, "image_dartY", image_path)
