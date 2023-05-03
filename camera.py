04428980dd52ef312b025b59eb3ae4b724d79a98
zkashef <zkashef@icloud.com>
2023-05-03 13:08:49 -0400

    help

--- camera.py
+++ camera.py
@@ -208,12 +208,12 @@ class Camera():
         ctrs, hier = cv2.findContours(edge, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
         # display images
-        plt.imshow(diff, cmap='gray')
-        plt.show()
-        plt.imshow(thresh, cmap='gray')
-        plt.show()
-        plt.imshow(edge, cmap='gray')
-        plt.show()
+        # plt.imshow(diff, cmap='gray')
+        # plt.show()
+        # plt.imshow(thresh, cmap='gray')
+        # plt.show()
+        # plt.imshow(edge, cmap='gray')
+        # plt.show()
         
         # find coordinates of dart tip in canny edge image
 
